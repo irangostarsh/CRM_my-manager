41e4711cf419ec1a99a0840b252fcbf19a633f97
irangostarsh <myrchyrshya80@gmail.com>
2026-07-20 16:21:00 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
-function login() {
-
-    let user = document.getElementById("username").value;
-    let pass = document.getElementById("password").value;
-
-    if(user === "admin" && pass === "1234"){
-        window.location.href = "dashboard.html";
-    }else{
-        document.getElementById("msg").innerHTML = "نام کاربری یا رمز عبور اشتباه است!";
-    }
-
-}
+function login() 
+{ let user = document.getElementById("username").value;
+     let pass = document.getElementById("password").value;
+      if(user === "admin" && pass === "1234"){ window.location.href = "dashboard.html"; }
+      else{ document.getElementById("msg").innerHTML = "نام کاربری یا رمز عبور اشتباه است!"; } }
